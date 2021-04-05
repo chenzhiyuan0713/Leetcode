4ac98ac9f143b40464198adccaa991bfdd9e88db
chenzhiyuan0713 <zchen82-c@my.cityu.edu.hk>
2021-04-06 01:17:37 +0800

    # add some question

--- Easy/Q35.py
+++ Easy/Q35.py
@@ -26,40 +26,99 @@
 """
 
 
-class Solution:
+# class Solution:
+#     def countGoodTriplets(self, arr, a: int, b: int, c: int) -> int:
+#         result_counter = 0
+#         for index in range(len(arr)):
+#             counter = 1
+#             while index+counter < len(arr):
+#                 if abs(arr[index] - arr[index + counter]) <= a:
+#                     # print(arr[index], arr[index + counter])
+#                     counter_2 = 1
+#                     # counter += 1
+#                     counter_3 = 0
+#                     while index + counter + counter_2 < len(arr):
+#                         if abs(arr[index + counter] - arr[index + counter + counter_2]) <= b:
+#                             # print(arr[index + counter], arr[index + counter + counter_2])
+#                             # counter_2 += 1
+#                             while index + counter + counter_2 + counter_3 < len(arr):
+#                                 if abs(arr[index] - arr[index + counter + counter_2 + counter_3]) <= c and abs(arr[index] - arr[index + counter + counter_2 + counter_3]) <= b:
+#                                     # print(arr[index + counter + counter_2], arr[index + counter + counter_2 + counter_3])
+#                                     result_counter += 1
+#                                     print(arr[index], arr[index + counter], arr[index + counter + counter_2 + counter_3],(index, index + counter, index + counter + counter_2 + counter_3))
+#                                     counter_3 += 1
+#                                 else:
+#                                     # print(arr[index],arr[index + counter + counter_2], arr[index + counter + counter_2 + counter_3])
+#                                     break
+#                             counter_2 += 1
+#                         else:
+#                             counter_2 += 1
+#                     counter += 1
+#                 else:
+#                     counter += 1
+#         return result_counter
+
+
+
+class Solution2:
     def countGoodTriplets(self, arr, a: int, b: int, c: int) -> int:
-        result_counter = 0
-        for index in range(len(arr)):
-            counter = 1
-            while index+counter < len(arr):
-                if abs(arr[index] - arr[index + counter]) <= a:
-                    # print(arr[index], arr[index + counter])
-                    counter_2 = 1
-                    # counter += 1
-                    counter_3 = 0
-                    while index + counter + counter_2 < len(arr):
-                        if abs(arr[index + counter] - arr[index + counter + counter_2]) <= b:
-                            # print(arr[index + counter], arr[index + counter + counter_2])
-                            # counter_2 += 1
-                            while index + counter + counter_2 + counter_3 < len(arr):
-                                if abs(arr[index] - arr[index + counter + counter_2 + counter_3]) <= c:
-                                    # print(arr[index + counter + counter_2], arr[index + counter + counter_2 + counter_3])
-                                    result_counter += 1
-                                    print(arr[index], arr[index + counter], arr[index + counter + counter_2 + counter_3],(index, index + counter, index + counter + counter_2 + counter_3))
-                                    counter_3 += 1
-                                else:
-                                    # print(arr[index],arr[index + counter + counter_2], arr[index + counter + counter_2 + counter_3])
-                                    counter_3 += 1
-                            counter_2 += 1
-                        else:
-                            counter_2 += 1
-                    counter += 1
-                else:
-                    counter += 1
-        return result_counter
+        n = len(arr)
+        result = 0
+        for index_i in range(n-2):
+            for index_j in range(index_i + 1, n-1):
+                if abs(arr[index_i] - arr[index_j]) <= a:
+                    for index_k in range(index_j + 1, n):
+                        if abs(arr[index_j] - arr[index_k]) <= b and abs(arr[index_i] - arr[index_k]) <= c:
+                            result += 1
+        return result
 
 
-answer = Solution()
+answer = Solution2()
 # print(answer.countGoodTriplets([3,0,1,1,9,7], 7, 2, 3))
 # print(answer.countGoodTriplets([1,1,2,2,3], 0, 0, 1))
-print(answer.countGoodTriplets([4,4,4,0,0,10,7,8,8], 8, 4, 6))
+# print(answer.countGoodTriplets([4,4,4,0,0,10,7,8,8], 8, 4, 6))
+# print(answer.countGoodTriplets([2,6,9,0,1,8], 3, 1, 2))
+
+#
+# 0 1 2
+# 0 1 3
+# 0 1 4
+# 0 1 6
+# 0 1 7
+# 0 1 8
+# 0 2 3
+# 0 2 4
+# 0 2 6
+# 0 2 7
+# 0 2 8
+# 0 3 4
+# 0 5 6
+# 0 5 7
+# 0 5 8
+# 0 6 7
+# 0 6 8
+# 0 7 8
+# 1 2 3
+# 1 2 4
+# 1 2 6
+# 1 2 7
+# 1 2 8
+# 1 3 4
+# 1 5 6
+# 1 5 7
+# 1 5 8
+# 1 6 7
+# 1 6 8
+# 1 7 8
+# 2 3 4
+# 2 5 6
+# 2 5 7
+# 2 5 8
+# 2 6 7
+# 2 6 8
+# 2 7 8
+# 5 6 7
+# 5 6 8
+# 5 7 8
+# 6 7 8
+# 41
